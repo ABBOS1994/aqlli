4239a0175ae8a37e093464144cb15c0d3e9f9a02
ab.iskandarov <ab.iskandarov@ipakyulibank.uz>
2025-02-26 19:03:49 +0500

    some db changes

--- models/index.js
+++ models/index.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose')
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
+.then(()=> console.log('DB ulandi'))
+.catch(()=> console.log('DB ulanmadi!'))
 
 module.exports = mongoose
