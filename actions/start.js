6b5fbc9ac829de91fd2d62ef82db597527c3becf
abbos <iskandarov.foto@gmail.com>
2025-05-16 16:28:42 +0500

    Clean reset. Removed old commits with secrets.

--- actions/start.js
+++
@@ -1,9 +0,0 @@
-const mainKeyboard = require('../helpers/mainKeyboard')
-
-module.exports = async (ctx) => {
-  return ctx.replyWithHTML(ctx.i18n.t('start.text', { name: ctx.user.name }), {
-    reply_markup: mainKeyboard(ctx),
-    disable_web_page_preview: true,
-    parse_mode: 'HTML'
-  })
-}
--- helpers/admin.js
+++
@@ -1,8 +0,0 @@
-const Markup = require('telegraf/markup')
-
-const backKeyboard = Markup.inlineKeyboard(
-  [Markup.callbackButton('‹ Назад', 'admin_back')],
-  { columns: 2 }
-).extra()
-
-module.exports = { backKeyboard }
